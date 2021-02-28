bb679409522362bef9d4d221ceab90be8118ca44
Shobhit Srivastava <shobhit3107@gmail.com>
2021-02-28 18:56:58 +0530

    Added create post modal open and close action

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 function loadAllPosts() {
   window.location.href = "./html/postslist.html";
 }
+
+function showCreatePost() {
+  var createPostModal = document.getElementById("createpost-pop-up-modal");
+  createPostModal.style.display = "flex";
+}
+
+function hideCreatePost() {
+  var createPostModal = document.getElementById("createpost-pop-up-modal");
+  createPostModal.style.display = "none";
+}
