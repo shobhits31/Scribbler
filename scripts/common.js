c07ebff873566207950d9d05687b15f7533ad796
Shobhit Srivastava <shobhit3107@gmail.com>
2021-02-28 16:36:25 +0530

    Added singin popup modal

--- scripts/common.js
+++ scripts/common.js
@@ -1,13 +1,24 @@
-var modal = document.getElementById("signup-pop-up-modal");
+var modal1 = document.getElementById("signup-pop-up-modal");
+var modal2 = document.getElementById("signin-pop-up-modal");
 
-var btnAction = document.getElementsByClassName("signup-btn")[0];
+var btnAction1 = document.getElementsByClassName("signup-btn")[0];
+var btnAction2 = document.getElementsByClassName("signin-btn")[0];
 
-var span = document.getElementsByClassName("modal-close")[0];
+var span1 = document.getElementsByClassName("modal-close")[0];
+var span2 = document.getElementsByClassName("modal-close")[1];
 
-btnAction.onclick = function () {
-  modal.style.display = "flex";
+btnAction1.onclick = function () {
+  modal1.style.display = "flex";
 };
 
-span.onclick = function () {
-  modal.style.display = "none";
+span1.onclick = function () {
+  modal1.style.display = "none";
+};
+
+btnAction2.onclick = function () {
+  modal2.style.display = "flex";
+};
+
+span2.onclick = function () {
+  modal2.style.display = "none";
 };
