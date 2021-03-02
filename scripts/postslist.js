a7ced308f11e6fbf7b839ee4459f5d403167858a
Shobhit Srivastava <shobhit3107@gmail.com>
2021-03-02 21:48:32 +0530

    Delete post modal added

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,3 +1,8 @@
 function showPost() {
   window.location.href = "../html/post.html";
 }
+
+function deletePost() {
+  var deleteModal = document.getElementById("delete-modal");
+  deleteModal.style.display = "flex";
+}
