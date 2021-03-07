97f80f73e31c96c15788ec85c0839ca2d2afef9d
Shobhit Srivastava <shobhit3107@gmail.com>
2021-03-07 21:37:23 +0530

    Updated code comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
+// Redirects to postslist page
 function loadAllPosts() {
   window.location.href = "./html/postslist.html";
 }
 
+// Displays Create Post modal
 function showCreatePost() {
   var createPostModal = document.getElementById("createpost-pop-up-modal");
   createPostModal.style.display = "flex";
 }
 
+// Closes Create Post modal
 function hideCreatePost() {
   var createPostModal = document.getElementById("createpost-pop-up-modal");
   createPostModal.style.display = "none";
